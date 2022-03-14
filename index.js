a55d189a4baa32bcbceadad314de1a6dd8567c8c
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2022-03-15 00:25:19 +0900

    Small JSX challenge

--- index.js
+++ index.js
@@ -62,3 +62,20 @@
 //     page,
 //     document.getElementById("root")
 // )
+
+// challenge:
+// const nav = (
+//     <nav>
+//         <h1>My Website is Awesome</h1>
+//         <ul>
+//             <li>Pricing</li>
+//             <li>About</li>
+//             <li>Contact</li>
+//         </ul>
+//     </nav>
+// )
+
+// ReactDOM.render(
+//     nav,
+//     document.getElementById("root")
+// )
